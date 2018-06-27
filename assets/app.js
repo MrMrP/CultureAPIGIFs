9bb8651bec065248fb6a21a59515ee0152d66f52
Marcus Price <marcusprice@MacBook-Pro.local>
2018-06-26 22:38:39 -0400

    update

---
+++ assets/app.js
@@ -0,0 +1,45 @@
+
+
+// Here are the initial buttons Gifs will be pulled from///
+var topics = ["Get At Me Bro", "Shoot Your Shot", "Winning", "For the Culture", "what"];
+
+function createButtons() {
+    //  $("#button-view").empty();
+    for (var i = 0; i < topics.length; i++) {
+        var t = $("<button>");
+        // t.addClass("culture-topics");
+        // t.attr("topic-name", topics[i]);
+        t.text(topics[i]);
+        $("#button-view").append(t);
+    }
+}
+createButtons()
+
+//On click of each button a list of GIFs will be displayed in the hmtl//
+$("#button-view").on("click", function () {
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topics[i] + "&api_key=l0vXKgwN3ptyVENPDtTqpcFgT8jf374Z&limit=10";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        .then(function (response) {
+            var results = response.data;
+            console.log(results)
+
+
+            for (var i = 0; i < results.length; i++) {
+                var imageUrl = results[i].images.original.url;
+                var image = $("<img>");
+                image.attr("src", imageUrl);
+                $("#content").prepend(image);
+            }
+
+        })
+})
+
+
+
+
+
+
